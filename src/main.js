df2c36127f85ca323271df0b48a5c5d60576cd18
Victor Martins Dias <victormartinsjob@gmail.com>
2026-09-05 12:38:37 -0300

    feat: setup initial project structure and HTML metadata

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import "./styles/reset.css";
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/utility.css";
+
+document.documentElement.classList.add("ready");
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      favicon: path.resolve(__dirname, "public", "assets", "favicon.png"),
+      inject: "body",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "assets"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
